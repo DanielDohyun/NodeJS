146def10f9ebc760462bc86b200527dfc9a176fa
Daniel Kim <kkdh1109@gmail.com>
2021-02-02 16:32:03 -0500

    make http request to weatherstackapi

--- weather/app.js
+++ weather/app.js
@@ -2,8 +2,16 @@ const request = require('request');
 
 const url = 'http://api.weatherstack.com/current?access_key=f75dec76090767ae2467c1b4b4e65ec6&query=toronto';
 
-request({ url: url }, (err, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
+// request({ url: url }, (err, response) => {
+//     const data = JSON.parse(response.body);
+//     console.log(data.current);
+// });
 
+//if you set json = true => gives response data as an object 
+// request({ url: url, json: true }, (err, response) => {
+//     console.log(response.body.current);
+// });
+
+request({url:url, json:true}, (err, res) => {
+    console.log(`${res.body.current.temperature} + ${res.body.current.precip}`)
 })
