4e056cf13f2c9241b2c9f77d0471431de78723c3
Daniel Kim <kkdh1109@gmail.com>
2021-02-03 13:12:45 -0500

    error handling

--- weather/app.js
+++ weather/app.js
@@ -9,9 +9,31 @@ const url = 'http://api.weatherstack.com/current?access_key=f75dec76090767ae2467
 
 //if you set json = true => gives response data as an object 
 // request({ url: url, json: true }, (err, response) => {
-//     console.log(response.body.current);
+//     if (err) {
+//         console.log('Unable to connect to weather service')
+//     } else if (response.body.error) {
+//         console.log('Unable to find location')
+//     } else {
+//         console.log(response.body.current);
+//     }
 // });
 
-request({url:url, json:true}, (err, res) => {
-    console.log(`${res.body.current.temperature} + ${res.body.current.precip}`)
+// request({url:url, json:true}, (err, res) => {
+//     console.log(`${res.body.current.temperature} + ${res.body.current.precip}`)
+// })
+
+
+const toronto = 'https://api.mapbox.com/geocoding/v5/mapbox.places/toronto.json?access_token=pk.eyJ1Ijoia2tkaDExMDkiLCJhIjoiY2trcHBpejNxMDc2bjJ2bXUwcW11dHZyMCJ9.84ME-PdBrbc6UtHTC8kmcQ&limit=1'
+
+request({ url: toronto, json: true }, (err, res) => {
+    if (err) { 
+        console.log('Unable to connect to geo service')
+    } else if (res.body.features.length === 0) {
+        console.log('Unable to find location')
+    } else {
+        // features is arr of 1 item
+        const latitude = res.body.features[0].center[1];
+        const longitude = res.body.features[0].center[0];
+        console.log('longitude is ' + longitude + 'latitude is ' + latitude)
+    }
 })
