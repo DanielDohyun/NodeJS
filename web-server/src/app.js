a5eab86a8e8f7997d13915a7a216a3f00c421720
Daniel Kim <kkdh1109@gmail.com>
2021-02-09 14:47:23 -0500

    setup the routes, and send response as html and json

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,20 +3,36 @@ const app = express();
 
 //root url
 app.get('/', (req, res) => {
-    res.send('Hello express');
+    res.send('<h1>Home</h1>');
 });
+
+app.get('/people', (req, res) => {
+    res.send([{
+        name: 'daniel',
+        age: '26'
+    },
+        {
+            name: 'mark',
+            age: '30'
+        }
+    ]);
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About page</h1>');
+});
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: `It's Sunny`,
+        location: 'toronto'
+    })
+})
 
 app.get('/help', (req, res) => {
     res.send('help page');
 });
 
-app.get('/about', (req, res) => {
-    res.send('about page');
-});
-
-app.get('/weather', (req, res) => {
-    res.send('weather page');
-});
 
 //starts the app
 app.listen(3000, () => {
