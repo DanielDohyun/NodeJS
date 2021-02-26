727a5c720579bbbedb4b1becb2134d1b7f82aeb3
Daniel Kim <kkdh1109@gmail.com>
2021-02-26 15:13:58 -0500

    create a form and add event listener to capture the input's value

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,20 @@
 console.log('client side js file is loaded')
 
-fetch('http://localhost:3000/weather?address=toronto').then(res => {
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+
+    fetch(`http://localhost:3000/weather?address=` + location ).then(res => {
     res.json().then((res) => {
         if (res.error) {
             return console.log(res.error)
         }
-        console.log(res);
         console.log(res.location);
         console.log(res.forecast);
     })
+})
 })
