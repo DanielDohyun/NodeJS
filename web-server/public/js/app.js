02ab055782c3816785bbfca9008cf0fca6546062
Daniel Kim <kkdh1109@gmail.com>
2021-02-26 16:08:38 -0500

    style input and search button

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,19 +2,23 @@ console.log('client side js file is loaded')
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const msgOne = document.querySelector('.location');
+const msgTwo = document.querySelector('.forecast');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const location = search.value;
+    msgOne.textContent = 'Loading...';
+    msgTwo.textContent = '';
 
     fetch(`http://localhost:3000/weather?address=` + location ).then(res => {
     res.json().then((res) => {
         if (res.error) {
-            return console.log(res.error)
+            return msgOne.textContent = res.error
         }
-        console.log(res.location);
-        console.log(res.forecast);
+        msgOne.textContent = res.location;
+        msgTwo.textContent = res.forecast;
     })
 })
 })
