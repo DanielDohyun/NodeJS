8370dab0aca9936c527e60bd28a395d0116542ec
Daniel Kim <kkdh1109@gmail.com>
2021-03-10 15:32:17 -0500

    learn about fetch method

---
+++ task-manager/Create.js
@@ -0,0 +1,60 @@
+//create
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to database')
+    }
+    
+    const db = client.db(databaseName);
+
+    // db.collection('users').insertOne({
+    //     name: 'daniel',
+    //     age: 26,
+    //     _id: id
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     //ops return result of array.
+    //     console.log(result.ops);
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Albert',
+    //         age: 22
+    //     },
+    //     {
+    //         name: 'Dave',
+    //         age: 29
+    //     }
+    // ], (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert doc')
+    //     }
+    //     console.log(result.ops);
+    // })
+
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Study',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'do exercise',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'take a nap',
+    //         completed: true
+    //     }
+    // ], (err, result) => {
+    //     if (err) {
+    //         return console.log('unable to insert docs')
+    //     }
+
+    //     console.log(result.ops);
+    // })
+
+
+})
--- task-manager/read.js
+++ task-manager/read.js
@@ -1,16 +1,3 @@
-// const mongodb = require('mongodb');
-// const MongoClient = mongodb.MongoClient;
-// const ObjectID = mongodb.ObjectID;
-
-const { MongoClient, ObjectID } = require('mongodb');
-
-const connectionURL = 'mongodb://127.0.0.1:27017';
-const databaseName = 'task-manager';
-
-// const id = new ObjectID();
-// console.log(id);
-// console.log(id.getTimestamp());
-
 //read
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
     if (err) {
