cf58c5edd556287ad21ccbf5af3094ec89ddf455
Daniel Kim <kkdh1109@gmail.com>
2021-03-23 21:24:33 -0400

    review promise chaining concept

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,12 +1,43 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        // resolve([1, 3, 5])
-        reject('Things went wrong')
-    }, 2000)
-})
+// const doWorkPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         // resolve([1, 3, 5])
+//         reject('Things went wrong')
+//     }, 2000)
+// })
 
-doWorkPromise.then((result) => {
-    console.log('success: ', result)
-}).catch((err) => {
-    console.log('error: ', err);
+// doWorkPromise.then((result) => {
+//     console.log('success: ', result)
+// }).catch((err) => {
+//     console.log('error: ', err);
+// })
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b);
+        }, 2000)
+    })
+}
+
+// add(1, 2).then((sum) => {
+//     console.log(sum);
+
+//     add(sum, 5).then((sum2 ) => {
+//         console.log(sum2);
+//     }).catch((e) => {
+//         console.log(e);
+//     })
+// }).catch((e) => {
+//     console.log(e);
+// })
+
+
+//promise Chaining
+add(1, 2).then((sum) => {
+    console.log(sum);
+    return add(sum, 3)
+}).then((sum2) => {
+    console.log(sum2)
+}).catch(e => {
+    console.log(e)
 })
