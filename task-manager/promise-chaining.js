803ebe514123bfb00c1d4e7e6dfe6d60edb25f8b
Daniel Kim <kkdh1109@gmail.com>
2021-04-05 15:05:06 -0400

    review delete and update fxn using async and await

--- task-manager/promise-chaining.js
+++ task-manager/promise-chaining.js
@@ -14,12 +14,37 @@ const Task = require('./src/models/task');
 //     console.log(e);
 // });
 
-Task.findByIdAndDelete('6051533ca161b001e49dd5cf').then((task) => {
-    console.log(task);
-    return Task.countDocuments({ completed: false })
-}).then((res) => {
-    console.log(res);
-}).catch((e) => {
-    console.log(e);
+// const updateAgeAndCount = async (id, age) => {
+//     const user = await User.findByIdAndUpdate(id, { age })
+//     const count = await User.countDocuments({ age })
+    
+//     return count
+// }
+
+// updateAgeAndCount('60514fd3248ef921e40d6e9b', 2).then((count) => {
+//     console.log(count);
+// }).catch(e => {
+//     console.log(e);
+// })
+
+// Task.findByIdAndDelete('6051533ca161b001e49dd5cf').then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false })
+// }).then((res) => {
+//     console.log(res);
+// }).catch((e) => {
+//     console.log(e);
+// });
+
+const deleteTaskAndCount = async (id) => {
+    const deleteTask = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('6057bb200c2cab2dd0b67737').then(count => {
+    console.log(count)
+}).catch(e => {
+    console.log(e)
 });
 
