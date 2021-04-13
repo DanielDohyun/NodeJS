ed1773f0d08dadf38da7e5d5d3b24db8b81d70e5
Daniel Kim <kkdh1109@gmail.com>
2021-04-13 14:43:16 -0400

    change some codes for task's patch endpoint so that middleware can perform before the changes been made to the database

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -48,7 +48,17 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        // find the task
+        const task = await Task.findById(req.params.id);
+
+        // alter the task properties
+        updates.forEach((update) => task[update] = req.body[update]);
+
+        // save the task
+        await task.save();
+
+        // const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
         
         if (!task) {
             return res.status(404).send()
