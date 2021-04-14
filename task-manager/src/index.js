de5d2ba9ea9c2938deec4026d907a76b56346204
Daniel Kim <kkdh1109@gmail.com>
2021-04-13 20:22:10 -0400

    create auth middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,6 +7,18 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next();        
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is currently unavailable due to the maintenance')
+// })
+
 //automatically parse incoming json to an object => can access it in the request handler
 app.use(express.json());
 app.use(userRouter);
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -2,7 +2,8 @@ const express = require('express')
 const { Mongoose } = require('mongoose')
 const { findByIdAndUpdate } = require('../models/user')
 const User = require('../models/user')
-const router = new express.Router()
+const router = new express.Router();
+const auth = require('../middleware/auth');
 
 router.post('/users', async (req, res) => {
     // console.log(req.body)
@@ -31,14 +32,8 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
-
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (e) {
-        res.status(500).send();
-    }
+router.get('/users/me', auth, async (req, res) => {
+   res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
